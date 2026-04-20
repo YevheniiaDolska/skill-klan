a9757826ef2a0f6aed1edfe26114e2c9e089ea53
JaneDo <jane.dolska@gmail.com>
2026-04-20 14:38:15 +0200

    Homework 3

---
+++ Homework_3/Variables/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        String name = "Eugenia";
+        int age = 32;
+        double height = 160.5;
+        boolean isWoman = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(isWoman);
+
+        }
+    }
+
---
+++ Homework_3/helloWorld/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    static void main(){
+        String text = "Hello, World!";
+        System.out.println(text);
+    }
+}
