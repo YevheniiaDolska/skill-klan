a42d11b51f4a94c893fdc6fc500beb63ccbd4ade
JaneDo <jane.dolska@gmail.com>
2026-04-21 10:20:52 +0200

    Homework redone

---
+++ Homework_3/Skill-klan/src/main/java/Homework_3/Variables.java
@@ -0,0 +1,17 @@
+package Homework_3;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Eugenia";
+        int age = 32;
+        double height = 1.6;
+        boolean isWoman = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(isWoman);
+
+    }
+}
+
--- Homework_3/helloWorld/src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    static void main(){
-        String text = "Hello, World!";
-        System.out.println(text);
-    }
-}
