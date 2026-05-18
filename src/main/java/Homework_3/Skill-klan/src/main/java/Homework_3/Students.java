986ccf9e9db9978b8762bfbffe3a7cfbc940b8ef
JaneDo <jane.dolska@gmail.com>
2026-05-18 10:32:49 +0200

    .gitignore fix

---
+++ src/main/java/Homework_3/Skill-klan/src/main/java/Homework_3/Students.java
@@ -0,0 +1,13 @@
+package Homework_4;
+
+//We create an array and go through it using for cycle
+
+public class Students {
+    public static void main(String[] args) {
+        String[] students = {"Kate", "Jack", "Pablo", "Lisa", "Rachel"};
+        for (int i = 0; i < students.length; i++){
+            System.out.println(students[i]);
+        }
+
+    }
+}
